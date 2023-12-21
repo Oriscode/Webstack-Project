5f9de43bf3c0a626ddb60ebd3453e72af913917a
Oriscode <isiakayusuf07@yahoo.com>
2023-12-21 15:51:39 -0800

    updating delete route

--- app/oauth2.py
+++ app/oauth2.py
@@ -37,7 +37,7 @@ def verify_access_token(token: str, credentials_exception):
         raise credentials_exception
     
     return token_data
-
+# Confirming user authorisation
 def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
     
